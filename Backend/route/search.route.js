057f9b1f063eb8ed7d3ec1133b793f41434e27d4
Rhythm Goyal <rhythmgoyal5@gmail.com>
2025-09-18 17:27:09 +0530

    changed serch router

--- Backend/route/search.route.js
+++ Backend/route/search.route.js
@@ -1,5 +1,9 @@
 import express from "express";
 import {searchUsers} from "../controller/search.controller.js";
+import { searchBooks } from "../controllers/search.controller.js";
+router.get("/search", searchBooks);
+
+
 const router = express.Router();
 
 // router.get("/",search);
